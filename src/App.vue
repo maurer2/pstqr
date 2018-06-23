ee4e5c2901caf50079b46985ab4ec4613066afc6
Martin <mr.mmaurer+work@gmail.com>
2018-06-24 00:47:24 +0200

    Vue init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <header class="header">
+      Header
+    </header>
+    <main class="main">
+      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    </main>
+    <footer class="footer">
+      Footer
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0;
+}
+</style>
