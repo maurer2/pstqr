00e4e08447625094dcc2bac5540a4874754b79f3
Martin <mr.mmaurer+work@gmail.com>
2018-11-17 23:53:00 +0100

    Added dummy localStorage stuff

--- src/components/LocalStorageClient.vue
+++ src/components/LocalStorageClient.vue
@@ -1,6 +1,15 @@
 <template>
   <section class="section localstorage">
     LocalStorage Component
+    <template v-if="localStorageIsSupported">
+      <div>
+        <button type="button" @click="setSavedNumbers">Set Dummy Values</button>
+        <button type="button" @click="getSavedNumbers">Get Saved numbers</button>
+      </div>
+      <template v-for="number in getSavedNumbers()">
+        <div :key="number">{{ number }} </div>
+      </template>
+    </template>
   </section>
 </template>
 
@@ -9,8 +18,37 @@
 
   @Component
   export default class LocalStorageClient extends Vue {
+    private localStorageIsSupported: boolean = false;
+    private uniqueKey: string = 'pstqr';
+
     constructor() {
       super();
+
+      try {
+        localStorage.setItem(this.uniqueKey + 'isSupported', 'isSupported');
+        localStorage.removeItem(this.uniqueKey + 'isSupported');
+
+        this.localStorageIsSupported = true;
+      } catch(e) {
+        this.localStorageIsSupported = false;
+      }
+    }
+
+    setSavedNumbers() {
+      if(!this.localStorageIsSupported) {
+        return
+      }
+      // dummy items
+      const numbers = ['12345678901234', '43210987654321'];
+      localStorage.setItem(this.uniqueKey + '-numbers',  JSON.stringify(numbers));
+    }
+
+    getSavedNumbers() {
+      const restoredNumbers = JSON.parse(localStorage.getItem(this.uniqueKey + '-numbers'));
+
+      console.log(restoredNumbers);
+
+      return restoredNumbers;
     }
   }
 </script>
