d0078a55d65a847fa1b210fe787b5903782c0e2b
Martin <mr.mmaurer+work@gmail.com>
2018-11-18 23:53:57 +0100

    Reworked number input validation logic

--- src/components/NumberInput.vue
+++ src/components/NumberInput.vue
@@ -1,20 +1,21 @@
 <template>
-  <form class="section form" @submit.prevent="calculateNewNumber()" autocomplete="off">
+  <form class="section form" @submit.prevent="calculateNumber" autocomplete="off" novalidate>
     <fieldset class="fields">
       <div class="form-row">
         <label class="form-label" for="number">Alte Nummer:</label>
         <input class="form-field" name="number" id="number" placeholder="7 bis 9 Zeichen notwendig" type="number"
-          autocomplete="off" v-model="number" />
+          autocomplete="off" pattern="[0-9]*" inputmode="numeric" v-model="number" />
         <p class="form-error" v-if="!isValidInput">
           Nummer muss 7 bis 9 Zeichen lang sein!
         </p>
       </div>
-      <div class="form-row" v-if="isValidInput">
-        <button type="submit" class="form-button" @click="$emit('newNumber', numberNew)">Submit</button>
+      <div class="form-row">
+        <button type="submit" class="form-button">Submit</button>
       </div>
     </fieldset>
-    <div class="results" v-if="isValidInput">
-      Neue Nummer: <span>{{ numberCalculated }}</span>
+    <div class="results" v-if="isValidInput && numberCalculated.length > 0">
+      <span class="key">Neue Nummer:</span>
+      <span class="value">{{ numberCalculated }}</span>
     </div>
   </form>
 </template>
@@ -31,7 +32,7 @@
     // data
     private number: string = '';
     private numberCalculated: string = '';
-    private isValidInput: boolean = false;
+    private isValidInput: boolean = true;
     private numberConverter: PostnumberConverter;
 
     constructor() {
@@ -40,31 +41,28 @@
       this.numberConverter = new PostnumberConverter();
     }
 
-    @Watch('number')
-    private onNumberChanged(value: string) {
-      const isValidLength = (value.length >= 7 && value.length <= 9);
-      const isValidNumber = !Number.isNaN(parseInt(value, 10));
+    private calculateNumber() {
+      const isValidLength = (this.number.length >= 7 && this.number.length <= 9);
+      const isValidNumber = !Number.isNaN(parseInt(this.number, 10));
 
       if (isValidLength && isValidNumber) {
-        this.numberCalculated = this.numberConverter.getNewPostNumber(parseInt(value, 10));
-        this.isValidInput = true;
-        this.$store.commit('addCalculatedNumber', this.numberCalculated);
+          this.numberCalculated = this.numberConverter.getNewPostNumber(parseInt(this.number, 10));
+          this.isValidInput = true;
       } else {
+          this.numberCalculated = '';
           this.isValidInput = false;
-          this.$store.commit('addCalculatedNumber', '');
       }
     }
 
     @Watch('numberCalculated')
-    private numberCalculatedChanged(value: string) {
-      this.$emit('numberChanged', value);
+    private onNumberCalculatedChange() {
+      this.$store.commit('addCalculatedNumber', this.numberCalculated);
     }
   }
 </script>
 
 <style scoped>
   .form {
-    overflow: hidden;
     text-align: left;
   }
 
@@ -73,10 +71,6 @@
     padding: 0;
     margin: 0 0 1rem 0;
     min-width: 0;
-  }
-
-  .results {
-    margin: 0 0 1rem 0;
   }
 
   .form-row {
@@ -107,6 +101,24 @@
 
     &:empty {
       display: none;
+    }
+  }
+
+  .results {
+    padding: 1rem;
+    text-align: center;
+    color: var(--color-alpha) var(--color-gamma);
+
+    & .key {
+      display: inline-block;
+      padding: 0.25rem;
+      white-space: nowrap;
+    }
+
+    & .value {
+      display: inline-block;
+      padding: 0.25rem;
+      font-weight: bold;
     }
   }
 </style>
