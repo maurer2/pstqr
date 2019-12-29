7a7e081144070cadea46eb15af2b6cac58b098bb
Martin <mr.mmaurer+work@gmail.com>
2019-12-29 13:31:30 +0000

    Added workaround for select box icon

--- src/components/NumberSelect/NumberSelect.vue
+++ src/components/NumberSelect/NumberSelect.vue
@@ -7,9 +7,10 @@
       class="selectbox select-css"
       v-model="selectValue"
       @change="$emit('numberHasChanged', newBarcodeNumber)"
+      :style="{'background-image': 'url('+ dropdownIcon +')'}"
     >
       <option disabled value="">
-        Nummer auswählen
+        Nummer auswählen:
       </option>
       <option v-for="number in numbersList" :key="number">
         {{ number }}
@@ -36,6 +37,9 @@ export default class NumberSelect extends Vue {
 
   private newBarcodeNumber: string = this.selectedBarcodeNumber;
 
+  // eslint-disable-next-line
+  private dropdownIcon = require('./dropdown-icon.svg');
+
   private get selectValue(): string {
     return this.newBarcodeNumber;
   }
@@ -56,7 +60,6 @@ export default class NumberSelect extends Vue {
   .selectbox {
     border: 1px solid var(--color-delta);
     border-radius: 0;
-    background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%48414CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E');
     font-weight: normal;
   }
 
